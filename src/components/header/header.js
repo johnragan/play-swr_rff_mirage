45e75aa21532add4b74f2cd3da2f5c6ab22a9ebb
John Ragan <john.ragan@gmail.com>
2020-08-22 19:18:00 -0400

    Added some missing folders

---
+++ src/components/header/header.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <Menu />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Make a Walt Disney World Reservation
+        </Typography>
+        <Button color="inherit">My Disney Experience</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
---
+++ src/components/login/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+// TODO: Once you create this form, convert it to RFF
+// TODO: Fake out logins using Mirage
+export default function Login() {
+  return (
+    <Typography component="h1" variant="h5">
+      Sign in
+    </Typography>
+  );
+}
