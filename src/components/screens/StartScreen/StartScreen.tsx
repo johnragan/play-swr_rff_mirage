bcd2253e155807c0ddf662016e21d61f5dac5c12
John Ragan <john.ragan@gmail.com>
2020-08-29 07:44:16 -0400

    Added support for pushing login action to Header from StartScreen.

--- src/components/screens/StartScreen/StartScreen.tsx
+++ src/components/screens/StartScreen/StartScreen.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import epcot from "../../..//photos/Epcot_mono_5x2.png";
 import { Typography } from "@material-ui/core";
 
 import Header from "../../Header/Header";
 
 export default function StartScreen() {
+  const history = useHistory();
+  const link = "/login";
+  const goToLogin = (): void => {
+    history.push(`${link}`);
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      <Header onMDEClick={goToLogin} />
       <img src={epcot} className="App-epcot" alt="epcot" />
       <Typography variant="h6">Your Ultimate WDW Planning Resource</Typography>
     </React.Fragment>
