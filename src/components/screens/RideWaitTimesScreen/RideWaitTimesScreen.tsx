5e0099c29e891e3fad65522e72176ba5b040be02
John Ragan <john.ragan@gmail.com>
2020-08-30 16:25:45 -0400

    Tried to get it let me pass an array of ride wait times, but running into some map and TS issues.

--- src/components/screens/RideWaitTimesScreen/RideWaitTimesScreen.tsx
+++ src/components/screens/RideWaitTimesScreen/RideWaitTimesScreen.tsx
@@ -3,14 +3,37 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import WaitTimesCard from "./WaitTimeCard/WaitTimesCard";
 
+const RenderCards = (
+  // @ts-ignore
+  rides //: [{ ride: string; land: string; waitMinutes: string }]
+) => {
+  rides = [
+    {
+      ride: "Living with the Land",
+      land: "Epcot",
+      waitMinutes: "15",
+    },
+  ];
+  return (
+    <React.Fragment>
+      {rides.map((ride: any) => (
+        <WaitTimesCard {...ride} />
+      ))}
+    </React.Fragment>
+  );
+};
+
 type Props = {};
 
 const RideWaitTimesScreen: React.FC<Props> = () => {
-  const epcotRide = {
-    ride: "Living with the Land",
-    land: "Epcot",
-    waitMinutes: "15",
-  };
+  const epcotRides = [
+    {
+      ride: "Living with the Land",
+      land: "Epcot",
+      waitMinutes: "15",
+    },
+  ];
+
   return (
     <React.Fragment>
       <Typography component="h1" variant="h4">
@@ -19,7 +42,9 @@ const RideWaitTimesScreen: React.FC<Props> = () => {
       <Typography component="h1" variant="h5">
         The following are the wait times for Epcot:
       </Typography>
-      <WaitTimesCard {...epcotRide} />
+      <WaitTimesCard {...epcotRides[0]} />
+      {/* @ts-ignore */}
+      <RenderCards rides={epcotRides} />
     </React.Fragment>
   );
 };
